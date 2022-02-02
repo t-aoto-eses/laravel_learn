1a84c2d72dcbd6a3fa10107ad6bcb4473b91f732
t-aoto-eses <t.aoto@eses-inc.jp>
2022-02-02 18:15:24 +0900

    routingを作成した。

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//ブログ一覧画面を表示
+Route::get('/', 'BlogController@showList')->name('blogs');
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
