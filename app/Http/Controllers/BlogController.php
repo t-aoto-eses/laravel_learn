af8c66c9d42698388ca7bbae4540d6967747988e
t-aoto-eses <t.aoto@eses-inc.jp>
2022-02-03 16:44:32 +0900

    データベースのデータを表示できるようにした。

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Blog;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
@@ -13,6 +14,10 @@ class BlogController extends Controller
      */
     public function showList()
     {
-        return view('blog.list');
+        $blogs = Blog::all();
+
+        // dd($blogs);
+
+        return view('blog.list', ['blogs' => $blogs]);
     }
 }
--- resources/views/blog/list.blade.php
+++ resources/views/blog/list.blade.php
@@ -2,21 +2,23 @@
 @section('title', 'ブログ一覧')
 @section('content')
 <div class="row">
-  <div class="col-md-8 col-md-offset-2">
+  <div class="col-md-12 py-5 col-md-offset-2">
     <h2>ブログ記事一覧</h2>
     <table class="table table-striped">
       <tr>
         <th>記事番号</th>
         <th>日付</th>
         <th>タイトル</th>
-        <th></th>
+        <th>内容</th>
       </tr>
+      @foreach($blogs as $blog)
       <tr>
-        <td>1</td>
-        <td>2020/06/30</td>
-        <td>テスト</td>
-        <td></td>
+        <td>{{ $blog->id }}</td>
+        <td>{{ $blog->updated_at }}</td>
+        <td>{{ $blog->title }}</td>
+        <td>{{ $blog->content }}</td>
       </tr>
+      @endforeach
     </table>
   </div>
 </div>
